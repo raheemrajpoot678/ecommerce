d55b2703159351e3a9615f02df6e78a4d65f0cc5
raheemrajpoot678 <raheemrajpoot678@gmail.com>
2023-12-22 19:18:10 +0500

    add asets

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return <div>HeaderComponent</div>
+}
+
+export default HeaderComponent
