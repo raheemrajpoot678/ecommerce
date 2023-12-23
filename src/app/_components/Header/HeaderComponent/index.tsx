b8eaa11e24892a2047773da4c453bb6017fbe5c9
raheemrajpoot678 <raheemrajpoot678@gmail.com>
2023-12-23 14:22:50 +0500

    implement Footer

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,8 +1,34 @@
+'use client'
+
 import React from 'react'
 import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import classes from './index.module.scss'
+import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
-  return <div>HeaderComponent</div>
+  const pathname = usePathname()
+  return (
+    <>
+      <nav
+        className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+          .filter(Boolean)
+          .join(' ')}
+      >
+        <Gutter className={classes.wrap}>
+          <Link href="/">
+            <Image src="/logo-black.svg" width={170} height={50} alt="logo" />
+          </Link>
+          <HeaderNav header={header} />
+        </Gutter>
+      </nav>
+    </>
+  )
 }
 
 export default HeaderComponent
