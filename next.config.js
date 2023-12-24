66004d384eb4378159fd61cd58671f4cae0e3a42
raheemrajpoot678 <raheemrajpoot678@gmail.com>
2023-12-24 16:20:43 +0500

    typescript

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
 const nextConfig = {
+  typescript: {
+    ignorBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
